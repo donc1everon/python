65b8d6f6e0c1e77f87a92661b756f7b96fbfad5e
donc1everon <it.antonov@gmail.com>
2018-07-10 09:35:41 +0300

    1st.2nd.ex.easy

--- les03/hw03_easy.py
+++ les03/hw03_easy.py
@@ -5,8 +5,8 @@
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
 def my_round(number, ndigits):
-    pass
-
+    r = int(number*(10**ndigits) + 0.5)/(10**ndigits)
+    return r
 
 print(my_round(2.1234567, 5))
 print(my_round(2.1999967, 5))
@@ -20,9 +20,23 @@ print(my_round(2.9999967, 5))
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
 def lucky_ticket(ticket_number):
-    pass
+    num_lst = []
+    i = 0
+    if 100000 <= ticket_number <= 999999:
+        while ticket_number > 0:
+            a = ticket_number % 10
+            num_lst.append(a)
+            ticket_number = ticket_number // 10
+            i += 1
+        if sum(num_lst[:3]) == sum(num_lst[3:]):
+            return 'У вас счастливый билет!!!'
+        else:
+            return 'Повезет в следующий раз.'
+    else:
+        return 'Не верный номер билета'
 
 
 print(lucky_ticket(123006))
 print(lucky_ticket(12321))
 print(lucky_ticket(436751))
+print(lucky_ticket(123219))
