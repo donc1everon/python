d4a984e7d3358b0e9a3041bf7f3b1f91432a5379
donc1everon <it.antonov@gmail.com>
2018-07-12 11:47:23 +0300

    2.ex.normal

--- les04/hw04_normal.py
+++ les04/hw04_normal.py
@@ -1,4 +1,21 @@
-﻿print(' Задание-1:\n'.center(55))
+﻿def output_sym(string, sym_lst, result_lst):
+    index = ''
+    for el in string:
+        for az in sym_lst:
+            if el != az:
+                continue
+            elif index == '':
+                el = ''
+            else:
+                result_lst.append(index)
+                el = ''
+                index = ''
+        index += el
+    result_lst.append(index)
+    return result_lst
+
+
+print(' Задание-1:\n'.center(55))
 # Вывести символы в нижнем регистре, которые находятся вокруг
 # 1 или более символов в верхнем регистре.
 # Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
@@ -23,27 +40,12 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
 result_re = re.findall(r'[^A-Z]+', line)
 
 # Способ № 2
-i = 0
-index = ''
 AZ_lst = list(map(chr, range(ord('A'), ord('Z')+1)))
 result_2 = []
 
-for el in line:
-       for AZ in AZ_lst:
-              if el != AZ:
-                     continue
-              elif index == '':
-                     el = ''
-              else:
-                     result_2.append(index)
-                     el = ''
-                     index = ''
-       index += el
-result_2.append(index)
+print('Символы в нижнем регистре ПРИ помоще re -', result_re)
+print('Символы в нижнем регистре БЕЗ помоще re -', output_sym(line, AZ_lst, result_2))
 
-
-print(result_re)
-print(result_2)
 
 print('\n', 'Задание-2:\n'.center(55))
 # Вывести символы в верхнем регистре, слева от которых находятся
@@ -68,6 +70,39 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'uXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQN'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
+
+# Способ № 1
+result_re = re.findall(r'[a-z]{2}([A-Z]+)[A-Z]{2}', line_2)
+
+# Способ № 2
+az_lst = list(map(chr, range(ord('a'), ord('z')+1)))
+some_lst = []
+result_2 = []
+i = 0
+
+output_sym(line_2, az_lst, some_lst)
+
+while len(some_lst) > i:
+    # находим в списке индекс элемента состоящего из символов в верхнем регистре
+    j = line_2.find(some_lst[i])
+    # находим длину элемента
+    len_up_lit = len(some_lst[i])
+    # два символа в нижнем регистре до
+    low_lit = line_2[(j-2):j]
+    # два символа в верхнем регистре после
+    up_lit = line_2[(j+len_up_lit-2):(j+len_up_lit)]
+    # отделяем от двух символов в верхнем регистре в конце элемента
+    some_lst[i] = line_2[j:(j + len_up_lit - 2)]
+    # проверяем условие задачи
+    if low_lit.islower() & up_lit.isupper():
+        result_2.append(some_lst[i])
+    i += 1
+
+result_2 = [el for el in result_2 if el != '']
+
+print('Символы в нижнем регистре ПРИ помоще re -', result_re)
+print('Символы в нижнем регистре БЕЗ помоще re -', result_2)
+
 
 print('\n', 'Задание-3:\n'.center(55))
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
