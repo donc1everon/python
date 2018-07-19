42e8b1a935986eb99d78b96757e188e16d102515
Ilya Antonov <it.antonov@gmail.com>
2018-07-19 22:09:12 +0300

    решена часть задачи 2 easy

--- les06/hw06_easy.py
+++ les06/hw06_easy.py
@@ -24,11 +24,38 @@ class Triangle:
 
 
 triangle1 = Triangle((1, 2), (3, 3), (2, 6))
-print(triangle1.AB, triangle1.BC, triangle1.CA, triangle1.find_square(), triangle1.find_height(),
-      triangle1.find_perimetr())
+print(f'Сторона АВ = {triangle1.AB}\nСторона BC = {triangle1.BC}\nСторона CA = {triangle1.CA}\n'
+      f'Периметр = {triangle1.find_perimetr()}\nПлощадь = {triangle1.find_square()}\n'
+      f'Высота = {triangle1.find_height()}')
 
 
 # Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
 # Предусмотреть в классе методы:
 # проверка, является ли фигура равнобочной трапецией;
 # вычисления: длины сторон, периметр, площадь.
+
+class EqualTrapezoid:
+    def __init__(self, point_a, point_b, point_c, point_d):
+        self.a = point_a
+        self.b = point_b
+        self.c = point_c
+        self.d = point_d
+
+    def sides(self):
+        self.AB = round(math.sqrt((self.b[0] - self.a[0]) ** 2 + (self.b[1] - self.a[1]) ** 2), 2)
+        self.BC = round(math.sqrt((self.c[0] - self.b[0]) ** 2 + (self.c[1] - self.b[1]) ** 2), 2)
+        self.CD = round(math.sqrt((self.d[0] - self.c[0]) ** 2 + (self.d[1] - self.c[1]) ** 2), 2)
+        self.DA = round(math.sqrt((self.a[0] - self.d[0]) ** 2 + (self.a[1] - self.d[1]) ** 2), 2)
+
+    def check_on_equal(self):
+        self.AC = round(math.sqrt((self.c[0] - self.a[0]) ** 2 + (self.c[1] - self.a[1]) ** 2), 2)
+        self.BD = round(math.sqrt((self.d[0] - self.b[0]) ** 2 + (self.d[1] - self.b[1]) ** 2), 2)
+
+        if self.AC == self.BD:
+            return 'Проверка на равнобокость трапеция прошла!'
+        else:
+            return 'Трапеция не равнобокая!'
+
+
+trapezoid = EqualTrapezoid((1, 1), (2, 2), (5, 2), (6, 1))
+print(trapezoid.check_on_equal())
